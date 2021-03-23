4273dc1d8ee8f2cb0dc006bb0da9bc5f325337f0
viacheslav <mr.v.e.romantsov@gmail.com>
2021-03-23 22:46:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200)
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.status(200)
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT ${PORT}`);
+})
