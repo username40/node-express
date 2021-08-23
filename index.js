cfbbdad399a50f72b3c6e348e5baab2f443ca726
username <mr.v.e.romantsov@gmail.com>
2021-08-23 21:34:28 +0300

    create if in navbar.hbs

--- index.js
+++ index.js
@@ -17,12 +17,26 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Главная',
+    isHome: true
+  })
 })
 
-app.get('/about', (req, res) => {
+app.get('/add', (req, res) => {
   res.status(200)
-  res.render('about')
+  res.render('add', {
+    title: 'Добавить курс',
+    isAdd: true
+  })
+})
+
+app.get('/courses', (req, res) => {
+  res.status(200)
+  res.render('courses', {
+    title: 'Курсы',
+    isCourses: true
+  })
 })
 
 app.use(express.static('public'))
